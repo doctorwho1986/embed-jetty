872254e27e1937018089fbd9207a71fcb0062bd3
doctorwho1986 <doctorwho1986@foxmail.com>
2014-07-28 23:52:22 +0800

    EmbeddedTomcat  test

--- src/test/java/com/github/embedjetty/EmbedJettyTest.java
+++ src/test/java/com/github/embedjetty/EmbedJettyTest.java
@@ -2,13 +2,67 @@ package com.github.embedjetty;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class EmbedJettyTest {
 
+public class EmbedJettyTest {
+	private String contextPath = "/jetty";
+	private String webAppRelativePath = "/src/test/resources/webapps/webapp";
 	@Test
 	public void testEmbedJettyStringString() {
-		fail("Not yet implemented");
+		
+		EmbedJetty jetty = new EmbedJetty(contextPath, webAppRelativePath);
+		jetty.addServlet(contextPath, "/js", new HttpServlet() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void doGet(HttpServletRequest req,
+					HttpServletResponse resp) throws ServletException,
+					IOException {
+				doPost(req, resp);
+			}
+
+			@Override
+			protected void doPost(HttpServletRequest req,
+					HttpServletResponse resp) throws ServletException,
+					IOException {
+				System.out.println("servlet post");
+			}
+			
+		});
+		
+		jetty.start();
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpUriRequest request = RequestBuilder.get().setUri("http://localhost:8080/jetty/js").build();
+		CloseableHttpResponse response = null;
+		try {
+			response = httpClient.execute(request);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(response.getStatusLine());
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+		jetty.stop();
 	}
 
 	@Test
