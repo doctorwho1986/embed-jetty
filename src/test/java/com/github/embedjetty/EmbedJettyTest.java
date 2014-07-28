37541d1658c5cd420d3bea71d3f6dc41de7f82fe
doctorwho1986 <doctorwho1986@foxmail.com>
2014-07-28 19:03:35 +0800

    embed jetty

---
+++ src/test/java/com/github/embedjetty/EmbedJettyTest.java
@@ -0,0 +1,34 @@
+package com.github.embedjetty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmbedJettyTest {
+
+	@Test
+	public void testEmbedJettyStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmbedJettyIntStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddServlet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+}
